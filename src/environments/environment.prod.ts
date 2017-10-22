91898745713cf99a4aefe45057300609eabf2d07
Chonlasith Jucksriporn <chonlasith@gmail.com>
2017-10-22 11:17:49 +0700

    Add firebase configuration to env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyB-KQr5gtClMdlR3YBkwAAftYPxznviVnc',
+    authDomain: 'robot-stage-1.firebaseapp.com',
+    databaseURL: 'https://robot-stage-1.firebaseio.com'
+  }
 };
